6a833d809bf6dda38cfa54f86a23261cb72a5810
Will <wjeckert15@gmail.com>
2020-12-14 15:02:40 -0600

    last changes

--- src/SummaryTotal.js
+++ src/SummaryTotal.js
@@ -12,7 +12,7 @@ function SummaryTotal(props) {
       );
   
     return (
-        <div className="summary__total" key = "summary total">
+        <div className="summary__total">
             <div className="summary__total__label">Total</div>
             <div className="summary__total__value">
                 {USCurrencyFormat.format(total)}
